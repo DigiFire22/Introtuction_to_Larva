a415f7740ea73fb83d557404a0dff14645e2f8d2
Opilane TTHK <opilane@tthk.ee>
2022-03-01 15:52:33 +0200

    initial

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/minu.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my_first_post">my first post</a></h1>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est dicta beatae maxime tenetur officia, cum consectetur inventore iusto laboriosam! Itaque adipisci repellendus sint illum delectus! Quasi quod debitis aut. Facere!
+        </p>
+    </article>
+    <article>
+        <h1><a href="/posts/my_second_post">my second post</a></h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt repellendus iure quasi? Soluta distinctio ut labore vel earum corporis dignissimos, totam id officia ab praesentium voluptatum, suscipit, magni nesciunt numquam.
+        </p>
+    </article>
+    <article>
+        <h1><a href="/posts/my_third_post">my third post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero fuga harum ea officiis possimus, quas facilis ducimus dolores culpa tempore. Vel eligendi hic esse officiis beatae placeat perferendis possimus molestiae!
+        </p>
+    </article>
+</body>
+</html>
